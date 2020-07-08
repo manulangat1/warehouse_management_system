7c3e6f510550efeeb56aa90acc82002ec762cb5c
manulangat <emmanuelthedeveloper@gmail.com>
2020-07-08 17:22:35 +0300

    ch:adding_user_auth_api:backend

---
+++ wms/store/api/auth_api.py
@@ -0,0 +1,44 @@
+
+#django imports
+from django.contrib.auth.models import User
+
+#local imports 
+from ..serializers import UserSerializer,RegisterSerializer,LoginSerializer
+
+#rest_framework import s
+from rest_framework import generics,permissions
+from rest_framework.response import Response
+from rest_framework.views import APIView
+#knox imports
+from knox.models import AuthToken
+class UserApi(generics.RetrieveAPIView):
+    permissions_classes = [
+        permissions.IsAuthenticated
+    ]
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+
+
+class RegisterApi(generics.GenericAPIView):
+    serializer_class = RegisterSerializer
+
+    def post(self,request,*args,**kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        return Response({
+            "user":UserSerializer(user,context=self.get_serializer_context()).data,
+            "token":AuthToken.objects.create(user)[1]
+        })
+
+class LoginSerializer(generics.GenericAPIView):
+    def post(self,request,*args,**kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data
+        return Response({
+            "user":UserSerializer(user,context=self.get_serializer_context()).data,
+            "token":AuthToken.objects.create(user)[1]
+        })
